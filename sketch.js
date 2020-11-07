c30051f7b60bb57e5c9be44f5808c36c05616f57
krushna2603 <72137985+krushna2603@users.noreply.github.com>
2020-11-07 17:01:56 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -80,7 +80,7 @@ if(keyCode === LEFT_ARROW){
 
 helicopterSprite.x=helicopterSprite.x-20;
 translation={x:20,y:0}
-
+Matter.Body.translate(packageBody,translation);
 }else if(keyCode===RIGHT_ARROW){
 helicopterSprite.x=helicopterSprite.x+20;
 translation={x:20,y:0}
